eea9f8b352761d839d12ea80923dba6e123cff90
Matias <mati.olea.thomse@gmail.com>
2023-09-16 23:53:00 -0400

    navbar nice funcional

---
+++ alien/script.js
@@ -0,0 +1,63 @@
+const viewportHeight = window.innerHeight;
+const viewportWidth = window.innerWidth;
+
+
+function init ()
+{
+
+    /* Funciones para la animación */
+    setTimeout(() => {
+        enter()
+    }, 1000);
+    function enter() {
+
+        const floor = document.getElementsByClassName('floor')[0];
+        floor.style.animation = 'piso 2.5s forwards';
+
+        const doors = document.getElementsByClassName('doors')[0];
+        doors.style.animation = 'acercarse 4s forwards';
+        
+        const left = document.getElementsByClassName('left')[0];
+        left.style.animation = 'open-doors 2s forwards';
+        left.style.animationDelay = " 1s";
+        
+        const right = document.getElementsByClassName('right')[0];
+        right.style.animation = 'open-doors 2s forwards';
+        right.style.animationDelay = " 1s";
+        
+        const espacio = document.getElementsByClassName('espacio')[0];
+        espacio.style.animation = 'espacio 2s forwards';
+        espacio.style.animationDelay = " 1s";
+        
+        setTimeout(() => {
+            const animationContainer = document.getElementsByClassName('animation')[0]; 
+            animationContainer.style.display = 'none';
+        }, 4000);
+    }
+
+
+    /* Funciones para la navbar */
+    var mobileMenuButton = document.getElementById("mobileMenuButton");
+    mobileMenuButton.onclick = function() {
+        document.getElementById("sideMenuHideOnMobile").classList.toggle("-translate-y-full");
+        document.getElementById("sideMenuHideOnMobile").classList.toggle("translate-y-28");
+        document.getElementById("sideMenuHideOnMobile").classList.toggle("mt-12");
+        document.getElementById("sideMenuHideOnMobile").classList.toggle("shadow-xl");
+        document.getElementById("mobileMenuButtonClose").classList.toggle("hidden");
+        document.getElementById("mobileMenuButtonOpen").classList.toggle("hidden");
+    }
+    // Hide element when click outside nav
+    var theElementContainer = document.getElementsByTagName("nav")[0];
+    document.addEventListener('click', function(event) {
+        if (!theElementContainer.contains(event.target)) {
+            document.getElementById("sideMenuHideOnMobile").classList.add("-translate-y-full");
+            document.getElementById("sideMenuHideOnMobile").classList.remove("mt-12");
+            document.getElementById("sideMenuHideOnMobile").classList.remove("shadow-xl");
+            document.getElementById("mobileMenuButtonOpen").classList.remove("hidden");
+            document.getElementById("mobileMenuButtonClose").classList.add("hidden");
+        }
+    });
+    
+  
+
+}
