545d57539125bb288063ec71272226ffceba8e84
Matias <mati.olea.thomse@gmail.com>
2023-09-23 00:14:29 -0400

    fixed tools page

--- script.js
+++ script.js
@@ -1,67 +1,3 @@
-const viewportHeight = window.innerHeight;
-const viewportWidth = window.innerWidth;
-
-
-function init ()
-{
-
-    /* Funciones para la animación */
-    setTimeout(() => {
-        enter()
-    }, 1000);
-    function enter() {
-
-        const floor = document.getElementsByClassName('floor')[0];
-        floor.style.animation = 'piso 2.5s forwards';
-
-        const doors = document.getElementsByClassName('doors')[0];
-        doors.style.animation = 'acercarse 4s forwards';
-        
-        const left = document.getElementsByClassName('left')[0];
-        left.style.animation = 'open-doors 2s forwards';
-        left.style.animationDelay = " 1s";
-        
-        const right = document.getElementsByClassName('right')[0];
-        right.style.animation = 'open-doors 2s forwards';
-        right.style.animationDelay = " 1s";
-        
-        const espacio = document.getElementsByClassName('espacio')[0];
-        espacio.style.animation = 'espacio 2s forwards';
-        espacio.style.animationDelay = " 1s";
-        
-        setTimeout(() => {
-            const animationContainer = document.getElementsByClassName('animation')[0]; 
-            animationContainer.style.display = 'none';
-        }, 4000);
-    }
-
-
-    /* Funciones para la navbar */
-    var mobileMenuButton = document.getElementById("mobileMenuButton");
-    mobileMenuButton.onclick = function() {
-        var navHeight = document.getElementById("mainNavBackground").clientHeight + 48
-        document.getElementById("mainMenuHideOnMobile").classList.toggle("-translate-y-full");
-        document.getElementById("mainMenuHideOnMobile").classList.toggle("translate-y-["+navHeight+"px]");
-        document.getElementById("mainMenuHideOnMobile").classList.toggle("mt-12");
-        document.getElementById("mainMenuHideOnMobile").classList.toggle("shadow-xl");
-        document.getElementById("mobileMenuButtonClose").classList.toggle("hidden");
-        document.getElementById("mobileMenuButtonOpen").classList.toggle("hidden");
-    }
-    // Hide element when click outside nav
-    var theElementContainer = document.getElementsByTagName("nav")[0];
-    document.addEventListener('click', function(event) {
-        if (!theElementContainer.contains(event.target)) {
-            document.getElementById("mainMenuHideOnMobile").classList.add("-translate-y-full");
-            document.getElementById("mainMenuHideOnMobile").classList.remove("mt-12");
-            document.getElementById("mainMenuHideOnMobile").classList.remove("shadow-xl");
-            document.getElementById("mobileMenuButtonOpen").classList.remove("hidden");
-            document.getElementById("mobileMenuButtonClose").classList.add("hidden");
-        }
-    });
-    
-   
-
-}
 
 var styleSheetNumber;
 document.addEventListener("DOMContentLoaded", () => {
@@ -91,4 +27,42 @@ function toggleHiddenDescription(evt) {
         document.styleSheets[styleSheetNumber].cssRules[3].style.filter = "none" 
     }
     news.classList.toggle("hidden");
+}
+
+
+function fuelCalculator() {
+    // Get elements by name
+    var fuelType = document.getElementsByName("fuelType")[0]; 
+    var quantumFluxFactor = document.getElementsByName("quantumFluxFactor")[0]; 
+    var fuelConsumption = document.getElementsByName("fuelConsumption")[0]; 
+    var distance = document.getElementsByName("distance")[0]; 
+
+    // Check if fuel type is "gasolina" (assuming it's a dropdown with "gasolina" as one of the options)
+    if (fuelType.value === "plasma") {
+        // Calculate the result
+        var result = (distance.value / fuelConsumption.value) * quantumFluxFactor.value * 0.618033 ;
+        // Update the result input field
+        document.getElementsByName("fuelResult")[0].value = result;
+    } else if (fuelType.value === "antimatter") {
+        var result = (distance.value / fuelConsumption.value) * quantumFluxFactor.value * 0.718281;
+    } else {
+        var result = (distance.value / fuelConsumption.value) * quantumFluxFactor.value * 42;
+    }
+
+    
+    document.getElementsByName("fuelResult")[0].value = Math.round(result,3); ;
+}
+
+function ageCalculator() {
+    // Get values from input fields
+    const originalAge = parseFloat(document.getElementById("originalAge").value);
+    const originalFactor = parseFloat(document.getElementById("originalFactor").value);
+    const actualFactor = parseFloat(document.getElementById("actualFactor").value);
+
+   
+    const actualAge = (originalAge / originalFactor) * actualFactor;
+
+   
+    // Display the result in the "Actual Age" input field
+    document.getElementById("actualAge").value = actualAge.toFixed(2);
 }
